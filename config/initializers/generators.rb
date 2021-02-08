14f83778c9ae26dc931ec0058a68819848eecef6
yuukii125 <yuu01255@gmail.com>
2021-02-08 22:53:26 +0900

    環境構築

---
+++ config/initializers/generators.rb
@@ -0,0 +1,8 @@
+Rails.application.config.generators do |g|
+  g.helper false
+  g.stylesheets false
+  g.javascripts true
+  g.template_engine :erb
+  g.skip_routes true
+  g.test_framework false
+end
