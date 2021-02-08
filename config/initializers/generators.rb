a202ef048efe7c20082975415033828f121aff81
yuukii125 <yuu01255@gmail.com>
2021-02-08 23:21:24 +0900

    devise実装

--- config/initializers/generators.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.config.generators do |g|
-  g.helper false
-  g.stylesheets false
-  g.javascripts true
-  g.template_engine :erb
-  g.skip_routes true
-  g.test_framework false
-end
